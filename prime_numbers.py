c2e272f89f69fa8ee62be7de5c1a6532e8b42854
kwahalf <kwanusud@gmail.com>
2017-05-09 04:15:17 +0300

    prime number function with reduced variables debuged

--- prime_numbers.py
+++ prime_numbers.py
@@ -3,7 +3,7 @@ start = timer()
 def prime_numbers(n):
     out = list()
     for x in range(1, n+1):
-        if (x % i != 0 for i in range (2, int(x**.5)+1)):
+        if all(x % i != 0 for i in range (2, int(x**.5)+1)):
             out.append(x)
     return out
 end = timer()
